6dcd2882484b382d486d17ebb6fb8c20deefbaf0
Markus Klein <masmiseim@gmx.de>
2025-08-14 14:38:27 +0200

    rename support package to iMXRText (for extended) to be able to have the original and this modified version installed in parallel

--- _clean.bat
+++ _clean.bat
@@ -1,39 +1,11 @@
 del *.bak /S
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT1011_QuadSPI_4KB_SEC Debug"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT1011_QuadSPI_4KB_SEC Release"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT1015_QuadSPI_4KB_SEC Debug"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT1015_QuadSPI_4KB_SEC Release"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT1021_QuadSPI_4KB_SEC Debug"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT1021_QuadSPI_4KB_SEC Release"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT105x_QuadSPI_4KB_SEC Debug"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT105x_QuadSPI_4KB_SEC Release"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT105x_HYPER_256KB_SEC Debug"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT105x_HYPER_256KB_SEC Release"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT105x_EcoXiP_4KB_SEC Debug"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT105x_EcoXiP_4KB_SEC Release"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT106x_HYPER_256KB_SEC Debug"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT106x_HYPER_256KB_SEC Release"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT106x_QSPI_4KB_SEC Debug"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT106x_QSPI_4KB_SEC Release"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT106x_EcoXiP_4KB_SEC Debug"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT106x_EcoXiP_4KB_SEC Release"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT1064_QSPI_4KB_SEC Debug"
-rmdir /s /q "./targets/iMXRT/Loader/iMXRT Loader MIMXRT1064_QSPI_4KB_SEC Release"
+rmdir /s /q ".\targets\iMXRText\Loader2\out"
 
-rmdir /s /q "./targets\iMXRT\Loader2\out"
-
-
-rmdir /s /q "./samples/iMXRT/SayHello THUMB Debug"
-rmdir /s /q "./samples/iMXRT/SayHello THUMB Release"
-rmdir /s /q "./samples/iMXRT/DualCore_M4 THUMB Debug"
-rmdir /s /q "./samples/iMXRT/DualCore_M4 THUMB Release"
-rmdir /s /q "./samples/iMXRT/DualCore_M7 THUMB Debug"
-rmdir /s /q "./samples/iMXRT/DualCore_M7 THUMB Release"
-
-cd "./targets/iMXRT/Loader/"
+cd "./targets/iMXRText/Loader2/"
+del /f /q *_Debug.elf
 del /f /q *.map
 cd ..
+
 cd "./Loader2"
 del /f /q *.map
 del /f /q *.ind
